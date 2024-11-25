f1d429734bd7db486c6bd0afac8ff99a8a3bfef4
dounia-elg <dounia.elg20@gmail.com>
2024-11-25 23:22:25 +0100

    Instructions 1

---
+++ script.js
@@ -0,0 +1,29 @@
+/*Instructions 1 :
+
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+Starting with the original text, and given a character, return the text once it has been mutated so that all of the characters in the original text have been replaced with the character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+
+Solution : */
+
+function contamination(text, char){
+  
+    if (!text || !char) {
+          return "";
+      }
+    
+     return char.repeat(text.length);
+  }
+  console.log(contamination("abc","z"));
